5d011726fd8b3a635bba0b418d7be379fe5ddc2c
Robar3 <terehov94@mail.ru>
2022-08-26 19:00:11 +0300

    Initial commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        int lengthArr=15;
+        Test1 test1 =new Test1();
+        test1.minMaxAVG(lengthArr);
+    }
+
+ 
+}
